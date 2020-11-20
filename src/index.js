cf955d1cd1caf61b8286972f80ca89afd7be2250
mikohan <angara99@gmail.com>
2020-11-20 21:44:19 +0300

    Finished for today

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './styles.css'
-import { createStore } from './createStore'
+// import { createStore } from './createStore'
+import { createStore } from 'redux'
+import { rootReducer } from './redux/rootReducer'
+import { INCREMENT, DECREMENT } from './redux/types'
 
 const counter = document.getElementById('counter')
 const addBtn = document.getElementById('add')
@@ -7,16 +10,28 @@ const subBtn = document.getElementById('sub')
 const asyncBtn = document.getElementById('async')
 const themeBtn = document.getElementById('theme')
 
-const state = createStore()
 
-addBtn.addEventListener('click', () => {})
+const store = createStore(rootReducer, 0)
+window.store = store
+addBtn.addEventListener('click', () => {
+  store.dispatch({ type: INCREMENT })
+})
 
-subBtn.addEventListener('click', () => {})
+subBtn.addEventListener('click', () => {
+  store.dispatch({ type: DECREMENT })
+})
 
 addBtn.addEventListener('click', () => {})
 asyncBtn.addEventListener('click', async () => {
   setTimeout(() => {}, 2000)
 })
+
+
+store.subscribe(() => {
+  const state = store.getState()
+  counter.textContent = state;
+})
+store.dispatch({type: 'INIT_APPLICATION'})
 
 themeBtn.addEventListener('click', () => {
   // document.body.classList.toggle('dark')
