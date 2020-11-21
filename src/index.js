076c07f4d5fc1b435dff268dfc13f00478dcc2b8
mikohan <angara99@gmail.com>
2020-11-21 18:16:58 +0300

    Added diy logger

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import './styles.css'
 // import { createStore } from './createStore'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { rootReducer } from './redux/rootReducer'
 import { INCREMENT, DECREMENT } from './redux/types'
+import thunk from 'redux-thunk'
+import { asyncIncrement, decrement, increment } from './redux/actions'
 
 const counter = document.getElementById('counter')
 const addBtn = document.getElementById('add')
@@ -10,28 +12,38 @@ const subBtn = document.getElementById('sub')
 const asyncBtn = document.getElementById('async')
 const themeBtn = document.getElementById('theme')
 
+function logger(state) {
+  return function (next) {
+    return function (action) {
+      console.log('PreState:', state.getState())
+      console.log('Action:', action)
+      const newState = next(action)
+      console.log('PostState:', state.getState())
+      return newState
+    }
+  }
+}
 
-const store = createStore(rootReducer, 0)
+const store = createStore(rootReducer, 50, applyMiddleware(thunk, logger))
 window.store = store
 addBtn.addEventListener('click', () => {
-  store.dispatch({ type: INCREMENT })
+  store.dispatch(increment())
 })
 
 subBtn.addEventListener('click', () => {
-  store.dispatch({ type: DECREMENT })
+  store.dispatch(decrement())
 })
 
 addBtn.addEventListener('click', () => {})
 asyncBtn.addEventListener('click', async () => {
-  setTimeout(() => {}, 2000)
+  store.dispatch(asyncIncrement())
 })
-
 
 store.subscribe(() => {
   const state = store.getState()
-  counter.textContent = state;
+  counter.textContent = state
 })
-store.dispatch({type: 'INIT_APPLICATION'})
+store.dispatch({ type: 'INIT_APPLICATION' })
 
 themeBtn.addEventListener('click', () => {
   // document.body.classList.toggle('dark')
---
+++ src/redux/actions.js
@@ -0,0 +1,20 @@
+import * as types from './types'
+
+export function increment() {
+  return {
+    type: types.INCREMENT
+  }
+}
+export function decrement() {
+  return {
+    type: types.DECREMENT
+  }
+}
+
+export function asyncIncrement() {
+  return function (dispatch) {
+    setTimeout(() => {
+      dispatch({ type: types.ASYNC_INCREMENT })
+    }, 2000)
+  }
+}
